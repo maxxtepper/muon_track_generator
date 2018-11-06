51df17b7be91e7b2f4d7d308461640524104caa4
maxxtepper <maxxtepper@gmail.com>
2018-11-05 21:42:47 -0800

    Fixed typo in README instructions

---
+++ animate_2D_C.d
@@ -0,0 +1,98 @@
+
+# DO NOT DELETE
+
+./animate_2D_C.so: /usr/local/include/TFile.h
+./animate_2D_C.so: /usr/local/include/TDirectoryFile.h
+./animate_2D_C.so: /usr/local/include/TDirectory.h
+./animate_2D_C.so: /usr/local/include/TBuffer.h /usr/local/include/TObject.h
+./animate_2D_C.so: /usr/local/include/RConfigure.h
+./animate_2D_C.so: /usr/local/include/Rtypes.h
+./animate_2D_C.so: /usr/local/include/RtypesCore.h
+./animate_2D_C.so: /usr/local/include/ROOT/RConfig.h
+./animate_2D_C.so: /usr/local/include/RVersion.h
+./animate_2D_C.so: /usr/local/include/DllImport.h
+./animate_2D_C.so: /usr/local/include/snprintf.h /usr/local/include/strlcpy.h
+./animate_2D_C.so: /usr/local/include/TGenericClassInfo.h
+./animate_2D_C.so: /usr/local/include/TSchemaHelper.h
+./animate_2D_C.so: /usr/local/include/TIsAProxy.h
+./animate_2D_C.so: /usr/local/include/TVirtualIsAProxy.h
+./animate_2D_C.so: /usr/local/include/TStorage.h
+./animate_2D_C.so: /usr/local/include/TVersionCheck.h
+./animate_2D_C.so: /usr/local/include/RVersion.h /usr/local/include/TNamed.h
+./animate_2D_C.so: /usr/local/include/TString.h
+./animate_2D_C.so: /usr/local/include/TMathBase.h
+./animate_2D_C.so: /usr/local/include/ROOT/RStringView.hxx
+./animate_2D_C.so: /usr/local/include/ROOT/RWrap_libcpp_string_view.h
+./animate_2D_C.so: /usr/local/include/ROOT/libcpp_string_view.h
+./animate_2D_C.so: /usr/local/include/ROOT/TypeTraits.hxx
+./animate_2D_C.so: /usr/local/include/ROOT/RSpan.hxx
+./animate_2D_C.so: /usr/local/include/ROOT/span.hxx
+./animate_2D_C.so: /usr/local/include/TList.h
+./animate_2D_C.so: /usr/local/include/TSeqCollection.h
+./animate_2D_C.so: /usr/local/include/TCollection.h
+./animate_2D_C.so: /usr/local/include/TIterator.h
+./animate_2D_C.so: /usr/local/include/TVirtualRWMutex.h
+./animate_2D_C.so: /usr/local/include/TVirtualMutex.h
+./animate_2D_C.so: /usr/local/include/TDatime.h /usr/local/include/TUUID.h
+./animate_2D_C.so: /usr/local/include/TMap.h /usr/local/include/THashTable.h
+./animate_2D_C.so: /usr/local/include/TUrl.h
+./animate_2D_C.so: /usr/local/include/ROOT/RConcurrentHashColl.hxx
+./animate_2D_C.so: /usr/local/include/ROOT/TRWSpinLock.hxx
+./animate_2D_C.so: /usr/local/include/ROOT/TSpinMutex.hxx
+./animate_2D_C.so: /usr/local/include/TTree.h
+./animate_2D_C.so: /usr/local/include/ROOT/TIOFeatures.hxx
+./animate_2D_C.so: /usr/local/include/TBasket.h /usr/local/include/TKey.h
+./animate_2D_C.so: /usr/local/include/TClass.h
+./animate_2D_C.so: /usr/local/include/TDictionary.h
+./animate_2D_C.so: /usr/local/include/ESTLType.h
+./animate_2D_C.so: /usr/local/include/TObjArray.h
+./animate_2D_C.so: /usr/local/include/TObjString.h
+./animate_2D_C.so: /usr/local/include/ThreadLocalStorage.h
+./animate_2D_C.so: /usr/local/include/TArrayD.h /usr/local/include/TArray.h
+./animate_2D_C.so: /usr/local/include/TArrayI.h /usr/local/include/TAttFill.h
+./animate_2D_C.so: /usr/local/include/TAttLine.h
+./animate_2D_C.so: /usr/local/include/TAttMarker.h
+./animate_2D_C.so: /usr/local/include/TBranch.h
+./animate_2D_C.so: /usr/local/include/TDataType.h
+./animate_2D_C.so: /usr/local/include/TBranchCacheInfo.h
+./animate_2D_C.so: /usr/local/include/TBits.h
+./animate_2D_C.so: /usr/local/include/TVirtualTreePlayer.h
+./animate_2D_C.so: /usr/local/include/TChain.h /usr/local/include/TGraph.h
+./animate_2D_C.so: /usr/local/include/TVectorFfwd.h
+./animate_2D_C.so: /usr/local/include/TVectorDfwd.h
+./animate_2D_C.so: /usr/local/include/TFitResultPtr.h
+./animate_2D_C.so: /usr/local/include/TGraph2D.h
+./animate_2D_C.so: /usr/local/include/TVirtualHistPainter.h
+./animate_2D_C.so: /usr/local/include/TCanvas.h /usr/local/include/TPad.h
+./animate_2D_C.so: /usr/local/include/TVirtualPad.h
+./animate_2D_C.so: /usr/local/include/TAttPad.h
+./animate_2D_C.so: /usr/local/include/TVirtualX.h
+./animate_2D_C.so: /usr/local/include/TAttText.h
+./animate_2D_C.so: /usr/local/include/GuiTypes.h /usr/local/include/Buttons.h
+./animate_2D_C.so: /usr/local/include/TQObject.h /usr/local/include/TError.h
+./animate_2D_C.so: /usr/local/include/TVirtualQConnection.h
+./animate_2D_C.so: /usr/local/include/TInterpreter.h
+./animate_2D_C.so: /usr/local/include/TInterpreterValue.h
+./animate_2D_C.so: /usr/local/include/TAttBBox2D.h
+./animate_2D_C.so: /usr/local/include/TPoint.h
+./animate_2D_C.so: /usr/local/include/TAttCanvas.h
+./animate_2D_C.so: /usr/local/include/TCanvasImp.h
+./animate_2D_C.so: /usr/local/include/TStyle.h /usr/local/include/TAttAxis.h
+./animate_2D_C.so: /usr/local/include/TColor.h
+./animate_2D_C.so: /usr/local/include/TPolyMarker3D.h
+./animate_2D_C.so: /usr/local/include/TAtt3D.h /usr/local/include/TAxis.h
+./animate_2D_C.so: /usr/local/include/TSystem.h
+./animate_2D_C.so: /usr/local/include/TInetAddress.h
+./animate_2D_C.so: /usr/local/include/TTimer.h
+./animate_2D_C.so: /usr/local/include/TSysEvtHandler.h
+./animate_2D_C.so: /usr/local/include/TTime.h
+./animate_2D_C.so: /usr/local/include/TPolyLine3D.h /usr/local/include/TH3D.h
+./animate_2D_C.so: /usr/local/include/TH3.h /usr/local/include/TH1.h
+./animate_2D_C.so: /usr/local/include/TArrayC.h /usr/local/include/TArrayS.h
+./animate_2D_C.so: /usr/local/include/TArrayF.h /usr/local/include/Foption.h
+./animate_2D_C.so: /usr/local/include/Fit/FitExecutionPolicy.h
+./animate_2D_C.so: /usr/local/include/TH2D.h /usr/local/include/TH2.h
+./animate_2D_C.so: /usr/local/include/TMatrixFBasefwd.h
+./animate_2D_C.so: /usr/local/include/TMatrixDBasefwd.h
+./animate_2D_C.so: /usr/local/include/RVersion.h /usr/local/include/RConfig.h /usr/local/include/TClass.h /usr/local/include/TDictAttributeMap.h /usr/local/include/TInterpreter.h /usr/local/include/TROOT.h /usr/local/include/TBuffer.h /usr/local/include/TMemberInspector.h /usr/local/include/TError.h /usr/local/include/RtypesImp.h /usr/local/include/TIsAProxy.h /usr/local/include/TFileMergeInfo.h /usr/local/include/TCollectionProxyInfo.h /usr/local/bin/rootcling
+animate_2D_C__ROOTBUILDVERSION= 6.15/01
